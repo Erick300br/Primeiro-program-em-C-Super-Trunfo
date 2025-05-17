e5319e4b65e1f7c65dd2b7f91c612fb1d71bd324
Erick300br <erickaraujo300@gmail.com>
2025-05-17 21:30:30 +0000

    Atualização Super Trunfo 1.3.0

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
+    // Versão 1.3.0
 
     //carta 1
     char estado01[20];
@@ -8,19 +9,34 @@ int main(){
     char codigo01[5];
     //exemplo de código: A01
     char cidade01[20];
-    int pop01;
+    unsigned long int pop01;
     float areakm01;
     float pib01;
     int turistic01;
+    float densipop01;
+    float pibpercap01;
+    float power01;
+
 
  //carta 2
     char estado02[20];
     char codigo02[5];
     char cidade02[20];
-    int pop02;
+    unsigned long int pop02;
     float areakm02;
     float pib02;
     int turistic02;
+    float densipop02;
+    float pibpercap02;
+    float power02;
+
+    int resultpop;
+    int resultareakm;
+    int resultpib;
+    int resultturistic;
+    int resultdensipop;
+    int resultpibpercap;
+    int resultpower;
 
     printf("Seja bem vindo (a) ao jogo Super Trunfo!\n");
     printf("Vamos criar sua primeira carta!\n");
@@ -48,6 +64,9 @@ int main(){
 
     printf("Veja as informações da sua primeira carta: \n");
 
+    densipop01 = (double) pop01 / areakm01;
+    pibpercap01 = (double) pib01 / pop01;
+    power01 = pop01 + areakm01 + pib01 + turistic01 + densipop01 + pibpercap01;
     printf("Estado: %s\n", estado01);
     printf("Código: %s\n", codigo01);
     printf("Cidade: %s\n", cidade01);
@@ -55,6 +74,9 @@ int main(){
     printf("Area km2: %.2f\n", areakm01);
     printf("PIB: %.2f\n", pib01);
     printf("Pontos Turísticos: %d\n", turistic01);
+    printf("Densidade Populacional: %.2f hab/km2.\n", densipop01);
+    printf("PIB Per Capta: %.4f reais.\n", pibpercap01);
+    printf("Poder: %.3f.\n", power01);
 
 
     //SEGUNDA CARTA
@@ -85,6 +107,9 @@ int main(){
 
     printf("Veja as informações da sua segunda carta: \n");
 
+    densipop02 = (double) pop02 / areakm02;
+    pibpercap02 = (double) pib02 / pop02;
+    power02 = pop02 + areakm02 + pib02 + turistic02 + densipop02 + pibpercap02;
     printf("Estado: %s\n", estado02);
     printf("Código: %s\n", codigo02);
     printf("Nome: %s\n", cidade02);
@@ -92,6 +117,30 @@ int main(){
     printf("Area km2: %.2f\n", areakm02);
     printf("PIB: %.2f\n", pib02);
     printf("Pontos Turísticos: %d\n", turistic02);
+    printf("Densidade Populacional: %.2f hab/km2.\n", densipop02);
+    printf("PIB Per Capta: %.4f reais.\n", pibpercap02);
+    printf("Poder: %.3f.\n", power02);
+
+
+    printf("Hora da batalha!\n");
+    printf("Se o resultado for 1, a carta 1 venceu.\n");
+
+    resultpop = pop01 > pop02;
+    resultareakm = areakm01 > areakm02;
+    resultpib = pib01 > pib02;
+    resultturistic = turistic01 > turistic02;
+    resultdensipop = densipop01 > densipop02;
+    resultpibpercap = pibpercap01 > pibpercap02;
+    resultpower = power01 > power02;
+
+    printf("População: %d\n", resultpop);
+    printf("Areakm2: %d\n", resultareakm);
+    printf("PIB: %d\n", resultpib);
+    printf("Pontos Turísticos: %d\n", resultturistic);
+    printf("Densidade Populacional: %d\n", resultdensipop);
+    printf("PIB per Capita: %d\n", resultpibpercap);
+    printf("Poder: %d\n", resultpower);
+
     printf("FIM\n");
     return 0;
 }
